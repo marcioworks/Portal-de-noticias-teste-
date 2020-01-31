27e060b9af39699f5273e6458095af9faba49d1d
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-01-31 13:11:05 -0300

    body parser

--- app/routes/adicionarNoticia.js
+++ app/routes/adicionarNoticia.js
@@ -1,5 +1,10 @@
-module.exports = (app)=>{
-    app.get('/adicionar',(req,res)=>{
+module.exports = (app) => {
+    app.get('/adicionar', (req, res) => {
         res.render('admin/adicionarNoticia')
     });
+
+    app.post('/noticias/salvar', (req, res) => {
+        let noticia = req.body;
+        res.send(noticia)
+    })
 }
--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const consign = require('consign')
+const bodyparser = require('body-parser')
+
+
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views', './app/views')
+
+app.use(bodyparser.urlencoded({extended:true}))
 
 consign()
     .include('app/routes')
