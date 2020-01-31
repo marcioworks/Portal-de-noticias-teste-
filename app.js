7d343ce8ca20d29e6879cb2faa0f928d432b1e67
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-01-31 11:09:46 -0300

    consign routes

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 
 const app = require('./config/server')
 
-
-const rotaHome = require('./app/routes/home')(app)
-const rotaTecno = require('./app/routes/tecnologia')(app)
-const rotaModa = require('./app/routes/moda')(app)
-const rotaNoticias = require('./app/routes/noticias')(app)
-
-
-
-
-
+// const rotaHome = require('./app/routes/home')(app)
+// const rotaTecno = require('./app/routes/tecnologia')(app)
+// const rotaModa = require('./app/routes/moda')(app)
+// const rotaNoticias = require('./app/routes/noticias')(app)
 
 app.listen(3000, () => {
     console.log('Backend Running(Express)...')
